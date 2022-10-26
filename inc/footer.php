74a916330b1fb67d64e68ce4a1f2d386fa1e88e3
Marcos Alberto Oyarzo Alvarez <oyarzo.marcos97@outlook.com>
2022-10-26 20:52:17 -0300

    merge

---
+++ inc/footer.php
@@ -0,0 +1,6 @@
+
+  </div>
+<script type="text/javascript" src="<?php echo RUTA_URL ?>/js/main.js"></script>
+<script type="text/javascript" src="<?php echo RUTA_URL ?>/bootstrap/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>        
+</body>
+</html>
