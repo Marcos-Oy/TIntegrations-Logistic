99291f91ae3a2b903805c58ab305db9c3a8e01cd
clgaete <carlos.gaete23002@edu.ipchile.cl>
2022-10-26 23:06:50 -0300

    ArchivoBase
    Creación de archivos Base

---
+++ usuario.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <?php
+    include 'includes/plugins.php';
+    ?>
+</head>
+
+<body class="hold-transition sidebar-mini layout-fixed">
+    <div class="wrapper">
+        <?php 
+    include 'includes/header.php';
+    ?>
+
+        <div class="content-wrapper">
+            <div class="h1">Hola mundo 2</div>
+        </div>
+
+        <!-- Control Sidebar -->
+        <aside class="control-sidebar control-sidebar-dark">
+            <!-- Control sidebar content goes here -->
+        </aside>
+        <!-- /.control-sidebar -->
+    </div>
+    <?php
+    include 'includes/footer.php';
+    ?>
+</body>
