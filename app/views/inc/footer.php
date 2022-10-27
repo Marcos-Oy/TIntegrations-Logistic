c0ef144606f5ce555cc31c3fca6b773da613c7ce
Marcos Alberto Oyarzo Alvarez <oyarzo.marcos97@outlook.com>
2022-10-26 22:46:21 -0300

    rama limpia

--- app/views/inc/footer.php
+++
@@ -1,6 +0,0 @@
-
-  </div>
-<script type="text/javascript" src="<?php echo RUTA_URL ?>/js/main.js"></script>
-<script type="text/javascript" src="<?php echo RUTA_URL ?>/bootstrap/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>        
-</body>
-</html>
