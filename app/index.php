e74b62e298ffed88a8ee0210ea3cfe243eaf41c9
Marcos Alberto Oyarzo Alvarez <oyarzo.marcos97@outlook.com>
2022-10-26 21:16:16 -0300

    main limpia

--- app/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once 'config/config.php';
-
-require_once 'helpers/url_helper.php';
-//Auto load php
-spl_autoload_register(function($nombreClase){
-    require_once 'libraries/' .$nombreClase. '.php';
-});
