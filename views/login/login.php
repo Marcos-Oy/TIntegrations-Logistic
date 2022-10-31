2ebba1e023119219b8ee44ff0fbba7ea7a105552
clgaete <carlos.gaete23002@edu.ipchile.cl>
2022-10-31 16:57:54 -0300

    Cambio de email por text

--- views/login/login.php
+++ views/login/login.php
@@ -29,7 +29,7 @@
 
                 <form action="views/panel.php" method="post">
                     <div class="input-group mb-3">
-                        <input type="email" class="form-control" placeholder="Email">
+                        <input type="text" class="form-control" placeholder="Usuario">
                         <div class="input-group-append">
                             <div class="input-group-text">
                                 <span class="fas fa-envelope"></span>
