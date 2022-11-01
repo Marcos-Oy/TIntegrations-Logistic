f3972ac4307d32dd8ff0270d5fb250aa904ba1d2
clgaete <carlos.gaete23002@edu.ipchile.cl>
2022-10-31 21:50:54 -0300

    archivos base

--- views/ordenes/nueva.php
+++ views/ordenes/nueva.php
@@ -1,28 +1,34 @@
-<?php
+<!DOCTYPE html>
+<html lang="es">
 
-?>
-<!-- Content Header (Page header) -->
-<div class="content-header">
-    <div class="container-fluid">
-        <div class="row mb-2">
-            <div class="col-sm-6">
-                <h1 class="m-0">Nueva orden</h1>
-            </div><!-- /.col -->
-            <div class="col-sm-6">
-                <ol class="breadcrumb float-sm-right">
-                    <li class="breadcrumb-item"><a href="#">Inicio</a></li>
-                    <li class="breadcrumb-item active">Tablero Principal</li>
-                </ol>
-            </div><!-- /.col -->
-        </div><!-- /.row -->
-    </div><!-- /.container-fluid -->
-</div>
-<!-- /.content-header -->
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nueva Orden</title>
+    <?php
+    include '../includes/plugins.php';
+    ?>
+</head>
 
-<!-- Main content -->
-<div class="content">
-    <div class="container-fluid">
+<body class="hold-transition sidebar-mini layout-fixed">
+    <div class="wrapper">
+        <?php
+        include '../includes/header.php';
+        ?>
+        <div class="content-wrapper">
 
-    </div><!-- /.container-fluid -->
-</div>
-<!-- /.content -->
+        </div>
+        <!-- Control Sidebar -->
+        <aside class="control-sidebar control-sidebar-dark">
+            <!-- Control sidebar content goes here -->
+        </aside>
+        <!-- /.control-sidebar -->
+    </div>
+    <?php
+    include '../includes/footer.php';
+    ?>
+    <script src="js/validarRUT.js"></script>
+    <script type="text/javascript" src="js/order_new.js"></script>
+    
+</body>
