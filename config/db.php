6d4dfd8fae7eae191f46e67904aed82d7137f7ff
Marcos Alberto Oyarzo Alvarez <oyarzo.marcos97@outlook.com>
2022-11-05 21:08:19 -0300

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,20 +1,17 @@
 <?php
     class db{
-        private $host = "makrohard.sytes.net";
-        private $dbname = "tpqt2023";
-        private $user = "tpqt2023";
-        private $password = "pinturillo";
-
+        private $host= "makrohard.sytes.net";
+        private $dbname="tpqt2023";
+        private $user="tpqt2023";
+        private $password="pinturillo";
         public function conexion(){
             try{
-                $PDO = new PDO("mysql:host".$this->host.";dbname=".$this->dbname,$this->user,$this->password);
+                $PDO = new PDO("mysql:host=".$this->host.";dbname=".$this->dbname,$this->user,$this->password);
                 return $PDO;
-            }
-            catch(PDOException $e){
+            }catch(PDOException $e){
                 return $e->getMessage();
             }
         }
     }
-    $obj = new db();
-    print_r($obj->conexion());
+
 ?>
