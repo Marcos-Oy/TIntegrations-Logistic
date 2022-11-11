669227e6c915ad1f1a40a0afa05aa63404592d61
AyLeAv <aylinledezmaavalos@gmail.com>
2022-11-10 22:47:23 -0300

    cambios basico reci

--- app/controllers/Ordenes.Controller.php
+++ app/controllers/Ordenes.Controller.php
@@ -8,7 +8,13 @@
         public function NominarOrden() { require_once("resources/views/nominar/show.php");}
         public function AsignarOrden(){  require_once("resources/views/asignar/show.php");		}
         public function AsignarConCodigo()	{ require_once("resources/views/AsignarCodigo/show.php");	}
-        public function RecibirOrden(){  require_once("resources/views/recibir/show.php");	}
+        public function RecibirOrden()
+        {  
+            include("models/OrdenesModel.php");
+            $odtr = new OrdenesModel();
+            $result = $odtr->get_Solicitud();
+            require_once("resources/views/recibir/show.php");	
+        }
         public function RecibirOdtExterna(){  require_once("resources/views/CargarODTexterna/show.php");	}
         public function DevolverOrden(){  require_once("resources/views/devolver/show.php");	}
         public function EntregarOrden(){  require_once("resources/views/entregar/show.php");	}
--- resources/views/recibir/show.php
+++ resources/views/recibir/show.php
@@ -20,21 +20,21 @@
         ?>
         <div class="content-wrapper">
             <br>
-            <div class="h1 text-center text-primary">Recepcion de Ordenes</div>
+            <div class="h1 text-center text-info">Recepcion de Ordenes</div>
             <form method="post">
                 <div class="container text-center ">
                     <div class="input-group mb-3">
                         <input type="text" class="form-control" placeholder="Indique el Texto " aria-label="Username"
                             aria-describedby="basic-addon1">
                         &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
-                        <button type="button" class="btn btn-primary">
+                        <button type="button" class="btn btn-info">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                                 class="bi bi-search" viewBox="0 0 16 16">
                                 <path
                                     d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z">
                                 </path>
                             </svg>
-                            <font _mstmutation="1">BUSCAR</font> 
+                            <font _mstmutation="1">BUSCAR</font>
                         </button>
                         &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
                         <button type="button" class="btn btn-warning">LIMPIAR</button>
@@ -45,7 +45,7 @@
                 <div class="container text-center ">
                     <div class="input-group mb-2 ">
                         <input type="text" class="form-control" placeholder="Indique el Texto " aria-label="Username"
-                            aria-describedby="basic-addon1">&nbsp; &nbsp; 
+                            aria-describedby="basic-addon1">&nbsp; &nbsp;
                         <button type="button" class="btn btn-success">RECIBIR ENTREGA</button>
                     </div>
                 </div>
@@ -53,7 +53,7 @@
             <form method="POST" action="recibirorden.php">
                 <div class="container">
                     <div class="input-group mb-3">
-                        <button type="button" class="btn btn-primary col" name="entrega" value="Recibir Entrega">
+                        <button type="button" class="btn btn-info col" name="entrega" value="Recibir Entrega">
                             RECIBIR ENTREGA
                         </button>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
                         <button type="button" class="btn btn-success col " name="devolucion" value="Recibir Devolución">
@@ -63,24 +63,95 @@
                 </div>
                 <!-- Main content -->
                 <div class="container  table-responsive">
-                        <table class="table table-success  text-center">
-                            <thead>
-                                <tr>
-                                    <th></th>
-                                    <th>ODT</th>
-                                    <th>Fecha</tH>
-                                    <th>Región</th>
-                                    <th>Comuna</th>
-                                    <th>Bultos</th>
-                                    <th>Dimensiones</th>
-                                    <th>Peso</th>
-                                    <th>Cant. Sobres</th>
-                                    <th>Tipo de pago</th>
-                                    <th>Valor declarado</th>
-                                    <th>Total</th>
-                                </tr>
-                            </thead>
-                          
+                    <table class="table table-success  text-center">
+                        <thead>
+                            <tr>
+                                <th></th>
+                                <th>ODT</th>
+                                <th>Fecha</tH>
+                                <th>Región</th>
+                                <th>Comuna</th>
+                                <th>Bultos</th>
+                                <th>Dimensiones</th>
+                                <th>Peso</th>
+                                <th>Cant. Sobres</th>
+                                <th>Tipo de pago</th>
+                                <th>Valor declarado</th>
+                                <th>Total</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                                while ($orden = mysqli_fetch_array($result)) {
+                                $idorden = $orden[0];
+                                $fecha = $orden[1];
+                                $region = $orden[2];
+                                $comuna = $orden[3];
+                                $cantidad = $orden[4];
+                                $qsobres = $orden[5];
+                                $metros = round($orden[6]/1000000, 2);
+                                $peso = number_format($orden[7], 2, '.', '.');
+                                $valor = $orden[8];
+                                $documento = $orden[9];
+                                $valordeclarado = $orden[10];
+                                $tipopago = $orden[11];
+                                $idact = $orden[12]; 
+                            ?>
+                            <tr>
+                                <?php if ($_SESSION['cargoid']!=2) { ?>
+                                <td>
+                                    <div class="form-check">
+                                        <input class="form-check-input" onChange="marcar(this)" name="orden[]"
+                                            type="checkbox" value="<?php echo $idorden; ?>"
+                                            id="<?php echo $idorden; ?>">
+                                        <input type="hidden" value="<?php echo $valor; ?>"
+                                            id="valor<?php echo $idorden; ?>" name="valor<?php echo $idorden; ?>">
+                                        <input type='hidden' readonly class='form-control-plaintext text-center'
+                                            value='<?php echo $metros; ?>' id='metros<?php echo $idorden; ?>'
+                                            name='metros<?php echo $idorden; ?>'>
+                                        <input type='hidden' readonly class='form-control-plaintext text-center'
+                                            value='<?php echo $peso; ?>' id='peso<?php echo $idorden; ?>'
+                                            name='peso<?php echo $idorden; ?>'>
+                                        <input type='hidden' readonly class='form-control-plaintext text-center'
+                                            value='<?php echo $idact; ?>' id='actividad' name='actividad'>
+                                    </div>
+                                </td>
+                                <?php } 
+                                    if ($_SESSION['cargoid']!=2) 
+                                    {
+                                        echo "<tr>
+                                        <td>".$idorden."</td>
+                                        <td>".$fecha."</td>
+                                        <td>".$region."</td>
+                                        <td>".$comuna."</td>
+                                        <td>".$cantidad."</td>
+                                        <td>".$metros."</td>
+                                        <td>".$peso."</td>
+                                        <td>".$qsobres."</td>
+                                        <td>".$tipopago."</td>
+                                        <td>$".number_format((int)$valordeclarado, 0, ',', '.')."</td>
+                                        <td>$".number_format((int)$valor, 0, ',', '.')."</td>
+                                        </tr>";
+                                    } 
+                                    else 
+                                    {
+                                        echo" <tr>
+                                        <td>".$idorden."</td>
+                                        <td>".$fecha."</td>
+                                        <td>".$region."</td>
+                                        <td>".$provincia."</td>
+                                        <td>".$comuna."</td>
+                                        <td>".$cantidad."</td>
+                                        <td>".$metros."</td>
+                                        <td>".$peso."</td>
+                                        <td>".$qsobres."</td>
+                                        <td>".$tipopago."</td>
+                                        </tr>";
+                                    }
+                                }
+                                ?>
+                            
+                            </tbody>
                         </table>
                     </div>
                 
