e7325417fff4c5d7b8112ec5ba75cb08dfd950cb
clgaete <carlos.gaete23002@edu.ipchile.cl>
2022-11-15 21:53:57 -0300

    no se

--- resources/views/NuevaOrden/combos/listar_comunas.php
+++ resources/views/NuevaOrden/combos/listar_comunas.php
@@ -1,14 +1,27 @@
 <?php
-
+function getConn()
+{
+    $mysqli = mysqli_connect(
+        'makrohard.sytes.net',
+        'tpqtpint',
+        'pinturillo',
+        'tpqt2023'
+    );
+    if ($mysqli->connect_errno) {
+        echo "Fallo en la conexión de comunas con DB: ".mysqli_connect_error();
+    }
+    $mysqli->set_charset('utf8');
+    return $mysqli;
+}
 function getListaCom()
 {
     $id = $_POST['id'];
-    include '../';
-        $regionesM = new regionesModel();
-        $listaRegiones = $regionesM->show();
+    $mysqli = getConn();
+    $query = "CALL comunas_obtener(null,'$id')";
+    $result = $mysqli->query($query);
     $lista = "<option value =''>Seleccionar comuna</option>";
-    if (mysqli_num_rows($desccomuna) >= 1) {
-        while ($row = $desccomuna->fetch_array(MYSQLI_ASSOC)) {
+    if (mysqli_num_rows($result) >= 1) {
+        while ($row = $result->fetch_array(MYSQLI_ASSOC)) {
             $lista .= "<option value = '$row[id]'>$row[desc_comuna]</option>";
         }
         $mysqli->close();
