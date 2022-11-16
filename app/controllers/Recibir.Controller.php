7259ccb18efed19c2cf4b46f4dd050b35f642f8d
AyLeAv <aylinledezmaavalos@gmail.com>
2022-11-16 01:09:01 -0300

    RECIBIR FUNCIONADO
    FALTA EL LOGIN DE GONZALO

--- app/controllers/Recibir.Controller.php
+++ app/controllers/Recibir.Controller.php
@@ -13,18 +13,43 @@
 		
         public function Recibir()
 		{
+            include("models/OrdenesModel.php");
+            $obj = new OrdenesModel();
             date_default_timezone_set("America/Santiago");
             $fecha = date("Y-m-d H:i:s");
+            $count = 0;
             if (isset($_POST['entrega'])) {
                 if (!empty($_POST['ordenes'])) {
                     $checked_contador = count($_POST['ordenes']);
                     //echo $checked_contador."<br>";
+
                     foreach ($_POST['ordenes'] as $selected) 
                     {
                         //echo $selected."<br>";
                         //IN _numero bigint(25), IN _fecha text, IN _username text, IN _idbodega int
                         
+                        $obj->setUsername('ALEDAV');
+                        $obj->setODT($selected);
+                        $obj->setFecha($fecha);
+                        $obj->setBodega(3);
+                        
+                        $resultado = $obj->Recibir_Orden();
+                        if($resultado==true)
+                        {
+                            $count= $count+1;
+                            
+                        }
                     }
+                    if($checked_contador==$count)
+                        {
+                            echo "<script>alert('Ingreso Exitoso');
+                            window.location= '?control=Recibir&action=Principal'</script>";
+                        }
+                        else
+                        {
+                            echo "<script>alert('Ingreso No Exitoso');
+                            window.location= '?control=Recibir&action=Principal'</script>";
+                        }
 			
                 }
             }
--- models/OrdenesModel.php
+++ models/OrdenesModel.php
@@ -3,6 +3,53 @@
     {
         private $search ="";
         private $ODT;
+        private $username;
+        private $fecha;
+        private $bodega;
+
+        /************METODOS GET AND SET*************/
+        public function getODT()
+        {
+            return $this->ODT;
+        }
+        public function setODT($ODT): self
+        {
+            $this->ODT = $ODT;
+            return $this;
+        }
+        public function getUsername()
+        {
+            return $this->username;
+        }
+
+        public function setUsername($username): self
+        {
+            $this->username = $username;
+            return $this;
+        }
+        
+        public function getFecha()
+        {
+            return $this->fecha;
+        }
+        public function setFecha($fecha): self
+        {
+            $this->fecha = $fecha;
+            return $this;
+        }
+
+        public function getBodega()
+        {
+            return $this->bodega;
+        }
+
+        public function setBodega($bodega): self
+        {
+            $this->bodega = $bodega;
+
+            return $this;
+        }
+    
 
         public function __construct()
         {
@@ -21,11 +68,23 @@
 		/********************************* */
         public function Recibir_Orden()
 		{
-			$consulta="call Orden_Recibir()";
+			$consulta="call Orden_Recibir(".$this->ODT.", '".$this->fecha."', '".$this->username."',".$this->bodega.")";
 			$resultado=$this->basededatos->query($consulta);
-			return $resultado;
+			if($resultado==true)
+			{
+			return true;
+			}
+			else
+			{
+				return false;
+			}
 		}
         
-    }
+
+
+       
+
+       
+    }   
 
 ?>
