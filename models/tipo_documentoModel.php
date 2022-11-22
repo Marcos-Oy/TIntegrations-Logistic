ae7aac4e434a2ee1388e09aebc5315c8bdeb1c46
clgaete <carlos.gaete23002@edu.ipchile.cl>
2022-11-21 22:59:34 -0300

    actualizacion

--- models/tipo_documentoModel.php
+++ models/tipo_documentoModel.php
@@ -1,5 +1,7 @@
 <?php
     class tipoDocumentoModel{
+        private $id;
+        
         public function __construct()
     {
         require_once "config/db.php";
@@ -15,4 +17,33 @@
         return $this->tipodedocumentos;
         }
 
+        public function showById(){
+            $query = "call tipo_documento_obtener($this->id)";
+            $resultado = $this->basededatos->query($query);
+            while ($fila = $resultado->fetch_assoc()) {
+                $this->tipodedocumentos[] = $fila;
+            }
+        return $this->tipodedocumentos;
+        }
+
+
+        /**
+         * Get the value of id
+         */ 
+        public function getId()
+        {
+                return $this->id;
+        }
+
+        /**
+         * Set the value of id
+         *
+         * @return  self
+         */ 
+        public function setId($id)
+        {
+                $this->id = $id;
+
+                return $this;
+        }
     }
--- models/tipo_pagosModel.php
+++ models/tipo_pagosModel.php
@@ -2,11 +2,15 @@
 
 class tipoPagosModel
 {
+    private $id;
+
     public function __construct()
     {
         require_once "config/db.php";
         $this->basededatos = Conexion::conectar();
     }
+    
+    
     public function show()
     {
         $query = "call tipo_pago_obetener(null);";
@@ -16,5 +20,36 @@ class tipoPagosModel
         }
         return $this->tipopagos;
     
+    }
+
+    public function showById()
+    {
+        $query = "call tipo_pago_obetener($this->id);";
+        $resultado = $this->basededatos->query($query);
+        while ($fila = $resultado->fetch_assoc()) {
+            $this->tipopagos[] = $fila;
+        }
+        return $this->tipopagos;
+    
+    }
+
+    /**
+     * Get the value of id
+     */ 
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     *
+     * @return  self
+     */ 
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
     }
 }
